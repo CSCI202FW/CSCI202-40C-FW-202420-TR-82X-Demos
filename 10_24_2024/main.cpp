d776216e42f254430a47622570d54f97aab1d6bc
Angela Venable <avenable11@ivytech.edu>
2024-10-24 17:11:08 -0400

    final donut demo with pointers

--- 10_24_2024/main.cpp
+++ 10_24_2024/main.cpp
@@ -22,8 +22,8 @@ int main()
     while (true)
     {
         ice = getIcing();
-        // top = getTopping(ice);
-        // driz = getDrizzle();
+        top = getTopping(ice);
+        driz = getDrizzle();
         Donut **orderCpy;
         orderCpy = new Donut *[numDonuts];
         for (int i = 0; i < numDonuts; i++)
@@ -40,9 +40,33 @@ int main()
             delete orderCpy[i];
         }
         delete[] orderCpy;
-        // order[numDonuts - 1] = new Donut(ice, top, driz);
-        break;
+        try
+        {
+            order[numDonuts - 1] = new Donut(ice, top, driz);
+        }
+        catch (std::invalid_argument e)
+        {
+            std::cout << e.what();
+            break;
+        }
+        char again = 'Y';
+        std::cout << "Would you like to order more donuts (Y or N)? ";
+        std::cin >> again;
+        again = toupper(again);
+        if (again == 'N')
+        {
+            break;
+        }
     }
+    for (int i = 0; i < numDonuts; i++)
+    {
+        std::cout << order[i]->toString() << std::endl;
+    }
+    for (int i = 0; i < numDonuts; i++)
+    {
+        delete order[i];
+    }
+    delete[] order;
     return 0;
 }
 
@@ -76,6 +100,7 @@ std::string getIcing()
     std::transform(icingLower.begin(), icingLower.end(), icingLower.begin(), ::tolower);
     while (!Donut::strToIce.count(icingLower))
     {
+        codeGradeLoopFix("main.cpp::getIcing()");
         std::cout << ice << " is not a valid icing choice." << std::endl;
         std::cout << "Choose your icing from the choices below:" << std::endl;
         for (std::map<Donut::icingType, std::string>::iterator it = Donut::iceToStr.begin(); it != Donut::iceToStr.end(); ++it)
@@ -88,4 +113,55 @@ std::string getIcing()
         std::transform(icingLower.begin(), icingLower.end(), icingLower.begin(), ::tolower);
     }
     return ice;
+}
+std::string getTopping(std::string icing)
+{
+    std::string top;
+    std::cout << "Choose your topping from the choices below:" << std::endl;
+    std::transform(icing.begin(), icing.end(), icing.begin(), ::tolower);
+    if (icing == "none")
+    {
+        std::cout << "Cinnamon Sugar" << std::endl;
+        std::cout << "Powdered Sugar" << std::endl;
+        std::cout << "None" << std::endl;
+    }
+    else
+    {
+        for (auto it = Donut::topToStr.begin(); it != Donut::topToStr.end(); ++it)
+        {
+            std::cout << it->second << std::endl;
+        }
+    }
+    std::cin >> std::ws;
+    std::getline(std::cin, top);
+    std::string topCopy = top;
+    std::transform(topCopy.begin(), topCopy.end(), topCopy.begin(), ::tolower);
+    if (icing == "none")
+    {
+        while (topCopy != "cinnamon sugar" && topCopy != "powdered sugar" && topCopy != "none")
+        {
+            codeGradeLoopFix("main.cpp::getTopping(\"none\")");
+            std::cout << "Please choose a topping from the list" << std::endl;
+            std::getline(std::cin, top);
+            topCopy = top;
+            std::transform(topCopy.begin(), topCopy.end(), topCopy.begin(), ::tolower);
+        }
+    }
+    else
+    {
+        while (!Donut::strToTop.count(topCopy))
+        {
+            codeGradeLoopFix("main.cpp::getTopping()");
+            std::cout << "Please choose a topping from the list" << std::endl;
+            std::getline(std::cin, top);
+            topCopy = top;
+            std::transform(topCopy.begin(), topCopy.end(), topCopy.begin(), ::tolower);
+        }
+    }
+    return top;
+}
+
+std::string getDrizzle()
+{
+    return "none";
 }
